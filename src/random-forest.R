01e8bddc2b29dcae518f6a61be25be47c4ca9255
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-08-01 15:13:43 +0600

    Assess random-forest model on cross-validation only.

--- src/random-forest.R
+++ src/random-forest.R
@@ -3,34 +3,34 @@ source('transform-data.R')
 library(randomForest)
 # build random forest model
 ebayRandomForestModel <- randomForest(sold ~ . - description - UniqueID,
-                                      ntree=200,
+                                      ntree=5000,
                                       nodesize=1,
-                                      data=train)
+                                      data=newTrain)
 
 plot(ebayRandomForestModel)
 
 # plot ROC
 library(ROCR)
-predROCR <- prediction(predict(ebayRandomForestModel, type="prob", newdata=train)[,2],
-                      train$sold)
+predROCR <- prediction(predict(ebayRandomForestModel, type="prob", newdata=cvTrain)[,2],
+                       cvTrain$sold)
 perfROCR <- performance(predROCR, "tpr", "fpr")
 plot(perfROCR, colorize=TRUE, print.cutoffs.at=seq(0,1,0.1),
      text.adj=c(-0.2, 1.7))
 
 # Compute Accuracy
-print("train data Accuracy: ")
+print("cross-validation train data Accuracy: ")
 print(mean(performance(predROCR, "acc")@y.values[[1]]))
 
 # Compute Accuracy
-print("train data Sensitivity (true posititive rate): ")
+print("cross-validation train data Sensitivity (true posititive rate): ")
 print(mean(performance(predROCR, "sens")@y.values[[1]]))
 
 # Compute Accuracy
-print("train data Specificity (true negative rate): ")
+print("cross-validation train data Specificity (true negative rate): ")
 print(mean(performance(predROCR, "spec")@y.values[[1]]))
 
 # Compute AUC
-print("train data AUC: ")
+print("cross-validation train data AUC: ")
 print(performance(predROCR, "auc")@y.values[[1]])
 
 ## store result of prediction
