aa0c69f69add1e3ea87686f275306b8f8b45b759
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-08-02 10:12:09 +0600

    Make LR to use original version of data transformation.

--- src/logistic-regression.R
+++ src/logistic-regression.R
@@ -1,17 +1,15 @@
-#source('transform-data.R')
+source('transform-data.R')
 
 #newTrain <- newTrain[-c(1338, 146, 930, 1219, 1705, 1804, 1359),]
 
 # build logistic regression model
-logRegModel <- glm(formula = sold ~ biddable + startprice + condition_for_parts_or_not_working + 
-                     condition_seller_refurbished + cellular_0 + cellular_1 + 
-                     carrier_none + carrier_sprint + storage_16 + storage_32 + 
-                     productline_unknown + productline_ipad_1 + productline_ipad_2 + 
-                     productline_ipad_3 + productline_ipad_4 + productline_ipad_air + 
-                     productline_ipad_mini + productline_ipad_mini_2 + productline_ipad_mini_3 + 
-                     emptydescription - UniqueID, family = binomial(), data = newTrain)
+logRegModel <- glm(formula = sold ~ biddable + startprice + cellular + storage + productline + 
+                     emptydescription,
+                   family = binomial(),
+                   data = newTrain)
 
 plot(logRegModel)
+step(logRegModel)
 
 # test overdispersion
 print("ratio of residual deviance to the residual degreees of freedom for model: ")
@@ -23,29 +21,29 @@ predROCR <- prediction(predict(logRegModel, type="response", newdata=train),
                       train$sold)
 perfROCR <- performance(predROCR, "tpr", "fpr")
 plot(perfROCR, colorize=TRUE, print.cutoffs.at=seq(0,1,0.1),
-     text.adj=c(-0.2, 1.7))
+     text.adj=c(-0.2, 1.7), main="train data ROC")
+
+predROCR <- prediction(predict(logRegModel, type="response", newdata=cvTrain),
+                       cvTrain$sold)
+perfROCR <- performance(predROCR, "tpr", "fpr")
+plot(perfROCR, colorize=TRUE, print.cutoffs.at=seq(0,1,0.1),
+     text.adj=c(-0.2, 1.7), main="cross-validation train data ROC")
 
 # Compute Accuracy
-print("train data Accuracy: ")
+print("cross-validation train data Accuracy: ")
 print(mean(performance(predROCR, "acc")@y.values[[1]]))
 
 # Compute Accuracy
-print("train data Sensitivity (true posititive rate): ")
+print("cross-validation train data Sensitivity (true posititive rate): ")
 print(mean(performance(predROCR, "sens")@y.values[[1]]))
 
 # Compute Accuracy
-print("train data Specificity (true negative rate): ")
+print("cross-validation train data Specificity (true negative rate): ")
 print(mean(performance(predROCR, "spec")@y.values[[1]]))
 
 # Compute AUC
-print("train data AUC: ")
-print(performance(predROCR, "auc")@y.values[[1]])
-
-# AUC for cross-validated data
-cvROCR <- prediction(predict(logRegModel, type="response", newdata = cvTrain),
-                     cvTrain$sold)
 print("cross-validation train data AUC: ")
-print(performance(cvROCR, "auc")@y.values[[1]])
+print(performance(predROCR, "auc")@y.values[[1]])
 
 ## store result of prediction
 test1 <- test
